efc6b378d681c9c9505a47ec4b28f51c42725f07
v006 <v006@benner.com.br>
2018-07-12 15:44:35 -0300

    adicionando classe banco

---
+++ T1/Banco/Banco/Banco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco {
+    class Banco {
+
+        public Conta[] contas = new Conta[10];
+        public int indice = 0;
+
+
+        public void Adiciona(Conta conta) {
+
+
+            contas[indice] = conta;
+            indice++;
+        }
+    }
+}
