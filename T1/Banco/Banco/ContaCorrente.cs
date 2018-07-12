9a38267036c8b9c45ee0d2fc62b7f354a0823408
v006 <v006@benner.com.br>
2018-07-12 15:12:02 -0300

    adicionando conta corrente

---
+++ T1/Banco/Banco/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco {
+    class ContaCorrente :Conta {
+
+
+        public ContaCorrente(double saldo, Cliente titular, int numero) : base(saldo, titular, numero) {
+
+        }
+
+    }
+}
