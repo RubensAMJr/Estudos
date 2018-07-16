75c0f46d208d6b05722b426d96e8460bf9a3b285
v006 <v006@benner.com.br>
2018-07-16 17:19:27 -0300

    criando leitor de arquivos

---
+++ T1/Editor/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            if (File.Exists("entrada.txt")) {
+                Stream entrada = File.Open("entrada.txt", FileMode.Open);
+                StreamReader leitor = new StreamReader(entrada);
+
+                string linha = leitor.ReadToEnd();
+                texto.Text += linha;
+
+
+
+                //while (linha != null) {
+                //    texto.Text += linha;
+                 //   linha = leitor.ReadLine();
+                 //
+                //}
+                leitor.Close();
+                entrada.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Stream saida = File.Open("entrada.txt", FileMode.Create);
+            StreamWriter escritor = new StreamWriter(saida);
+            escritor.Write(texto.Text);
+
+            escritor.Close();
+            saida.Close();
+        }
+    }
+}
