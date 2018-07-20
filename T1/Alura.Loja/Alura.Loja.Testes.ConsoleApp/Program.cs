f602f04c25c52bfa71709c06246cf02f234eac72
v006 <v006@benner.com.br>
2018-07-20 14:23:16 -0300

    Adicionando exercicios de banco de dados

---
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new LojaContext()) {
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos) {
+                    Console.WriteLine(p);
+                }
+
+                var p1 = produtos.First();
+                p1.Nome = "Harry Potter";
+
+                contexto.SaveChanges();
+
+
+            }
+            
+        }
+
+        
+    }
+}
